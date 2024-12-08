5043853cd01b3bf18aa56d3cf58721f27f3ac449
Vivek Dhir <73019433+vivekdhir77@users.noreply.github.com>
2024-12-08 00:20:16 -0500

    Update server.js

--- be/server.js
+++ be/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", process.env.SERVER_URL],
+    origin: process.env.SERVER_URL || 'http://localhost:3000' ,
     credentials: true,
   })
 );
